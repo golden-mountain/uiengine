c5cf05a8b86723555c67e10bfe9aa226c24c365b
Jian Xu <xjian@a10networks.com>
2019-12-03 15:12:01 +0800

    fix data picker

--- src/plugins/data/data/picker.ts
+++ src/plugins/data/data/picker.ts
@@ -22,7 +22,36 @@ const execution: IPluginExecution = (param: IPluginParam) => {
 
   const dataSource = _.get(source, 'source')
   if (_.isString(dataSource) && dataSource) {
-    const route = getAccessRoute(dataSource).split('.')
+    const route: string[] = []
+    getAccessRoute(dataSource)
+      .split('.')
+      .forEach((slice: string) => {
+        const accessArray = /\[\d*\]/g;
+        const matchResult = slice.match(accessArray);
+        if (!_.isNil(matchResult)) {
+          let restStr = slice;
+          matchResult.forEach((matchStr: string) => {
+            const startIndex = restStr.indexOf(matchStr);
+            const endIndex = startIndex + matchStr.length;
+
+            route.push(restStr.slice(0, startIndex));
+            restStr = restStr.slice(endIndex);
+
+            const arrayIndex = /\[(\d*)\]/;
+            const mResult = matchStr.match(arrayIndex);
+            if (!_.isNil(mResult)) {
+              const indexStr = mResult[1];
+              const indexNum = Number(indexStr);
+              route.push(`${indexNum}`);
+            }
+          });
+          if (restStr) {
+            route.push(restStr);
+          }
+        } else {
+          route.push(slice);
+        }
+      })
     while (true) {
       const pickedData = _.get(data, route)
       if (pickedData !== undefined) {
