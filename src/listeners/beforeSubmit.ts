19f42f5d144ec5aa8cc5a65e45513e4d13b104ee
Jian Xu <xjian@a10networks.com>
2019-12-10 18:41:45 +0800

    add feedback

--- src/listeners/beforeSubmit.ts
+++ src/listeners/beforeSubmit.ts
@@ -1,8 +1,6 @@
 import _ from 'lodash'
 
-import {
-  NodeController
-} from '../data-layer'
+import { Feedback } from '../helpers/Feedback'
 
 import {
   IListener,
@@ -16,26 +14,68 @@ import {
   IUINode,
 } from '../../typings'
 
+function onProcessCompleted() {
+  let message: string = 'Submit Process Completed'
+  Feedback.getInstance().send('SubmitSuccess', message)
+}
+function onProcessFailed() {
+  let message: string = 'Submit Process Failed'
+  Feedback.getInstance().send('SubmitError', message)
+}
+function onProcessTerminated() {
+  let message: string = 'Submit Process Terminated'
+  Feedback.getInstance().send('SubmitError', message)
+}
+function onTargetSuccess() {
+  let message: string = 'Submit Target Succeeded'
+  Feedback.getInstance().send('SubmitSuccess', message)
+}
+function onTargetFailure() {
+  let message: string = 'Submit Target Failed'
+  Feedback.getInstance().send('SubmitError', message)
+}
+
 const listener: IListener = async (directParam: IListenerParam) => {
+  console.log('beforeSubmit begin')
 
   const uiNode: IUINode = _.get(directParam, 'uiNode')
   const target: ISubmitProcess | ISubmitTarget = _.get(directParam, 'target')
   const options: ISubmitOption = _.get(directParam, 'options')
   const callbacks: ISubmitCallback = _.get(directParam, 'callbacks')
 
-  return 'Submit Prepared'
+  let message: string = ''
+  if (!_.isNil(target)) {
+
+
+
+    if (_.isObject(options)) {
+      // if need customize
+      // options.requestGenerator =
+      // options.dependResolver =
+    }
+
+    if (_.isObject(callbacks)) {
+      callbacks.onProcessCompleted = onProcessCompleted
+      callbacks.onProcessFailed = onProcessFailed
+      callbacks.onProcessTerminated = onProcessTerminated
+      callbacks.onTargetSuccess = onTargetSuccess
+      callbacks.onTargetFailure = onTargetFailure
+    }
+
+    return 'Submit Prepared'
+  } else {
+    message = 'GUI Error: There is no submit config defined!'
+  }
+
+  Feedback.getInstance().send('SubmitError', message)
+  return 'Submit Stopped'
+
 }
 
 export const beforeSubmit: IListenerConfig = {
   name: 'beforeSubmit',
   paramKeys: ['uiNode', 'target', 'options', 'callbacks'],
-  debugList: [
-    'uiNode.id',
-    'uiNode.engineId',
-    'uiNode.layoutKey',
-    'target',
-    'options',
-  ],
+  debugList: [],
   listener,
   weight: 0,
   describe: {
